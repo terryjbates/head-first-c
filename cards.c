bed58e234a43a5f579b151f95935ed230a25cc8d
Terry Bates <terryjbates@gmail.com>
2013-01-30 22:23:24 -0500

    Completed exercise solution on page 37. Added code to exit loop if 'X' is entered, and diagnostic message if entered card is out of bounds.

--- cards.c
+++ cards.c
@@ -10,21 +10,36 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s",card_name);
-    int val = 0;
-    if (card_name[0] == 'K' ){
-        val = 10;
-    } else if (card_name[0] == 'Q' ){
-        val = 10;
-    } else if (card_name[0] == 'J' ){
-        val = 10;
-    } else if (card_name[0] == 'A' ){
-        val = 11;
-    }
-    else {
-        val = atoi(card_name);
-    }
-    printf("The card value is : %i\n", val);
-    return 0;
+    int count = 0;
+    while (card_name[0] != 'X'){
+        puts("Enter the card_name: ");
+        scanf("%2s",card_name);
+        int val = 0;
+        switch(card_name[0]){
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if ( (val < 1) || (val >10) ){
+                    puts("I don't understand that value!");
+                        continue;
+                }
+            }
+            if ((val > 2) && (val < 7)){
+                count++;
+            } else if (val == 10){
+                count--;
+            }
+            printf("Current count: %i\n", count);
+            
+        }
+        return 0;
 }
